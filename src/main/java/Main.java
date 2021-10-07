fad2524a4f16a677d1339846b5fc5e5b513c8066
Anton <anton.tseniukh@gmail.com>
2021-10-07 19:55:55 +0300

    add getById

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //GsonParser parser = new GsonParser();
-        SkillRepository skillRepository = new SkillRepository();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Skill skill = new Skill();
+        skill.setId(1);
+        skill.setName("Maven");
+        Skill skill2 = new Skill();
+        skill2.setId(2);
+        skill2.setName("MongoDB");
+        String json = gson.toJson(skill);
+        String json2 = gson.toJson(skill2);
 
+        System.out.println(json);
+
+
+
+        SkillRepository skillRepository = new SkillRepository();
         skillRepository.getAll();
-        //skillRepository.save(new Skill(1,"SQL"));
     }
 }
--- src/main/java/SkillRepository.java
+++ src/main/java/SkillRepository.java
@@ -1,39 +1,68 @@
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.File;
+
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SkillRepository {
-    private final Gson gson = new Gson();
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    private final String location = "";
+    private static final String LOCATION = "skills.json";
 
-    File file = new File("C:\\Users\\Anton\\Documents\\GitHub\\CRUD\\src\\main\\resources\\skills.json");
+    private URL resourseUrl = getClass().getClassLoader().getResource(LOCATION);
+
+    public Skill getSkillById(Integer id) {
+        try{
+            String skillJson = Files.readString(Path.of(resourseUrl.toURI()));
+            Type type = new TypeToken<ArrayList<Skill>>() {}.getType();
+            List<Skill> skills = gson.fromJson(skillJson,type);
+            for (Skill skill : skills){
+                if (skill.getId().equals(id)){
+                    return skill;
+                }
+            }
+        }catch (IOException | URISyntaxException e){
+            System.out.println("Exception " + e.toString());
+        }
+        return null;
+    }
 
     public List<Skill> getAll() {
-        try (FileReader reader = new FileReader(file)) {
-            List<Skill> skill = gson.fromJson(reader, new TypeToken<ArrayList<Skill>>() { }.getType());
-            return skill;
+        try {
+            String skillsJson = Files.readString(Path.of(resourseUrl.toURI()));
+            Type type = new TypeToken<ArrayList<Skill>>() {
+            }.getType();
+            List<Skill> skills = gson.fromJson(skillsJson, type);
+            for (Skill skill : skills) {
+                return skills;
+            }
         } catch (Exception e) {
             System.out.println("Exception " + e.toString());
         }
         return null;
     }
 
-    public Skill save(Skill skill){
+    public Skill save(Skill skill) {
 
-        try (FileReader reader = new FileReader(file)) {
-            String record  = gson.toJson(skill, new TypeToken<Skill>() {}.getType());
+        try {
+            String record = gson.toJson(skill, new TypeToken<Skill>() {
+            }.getType());
             return skill;
         } catch (Exception e) {
             System.out.println("Exception " + e.toString());
         }
         return null;
     }
-
 
 }
