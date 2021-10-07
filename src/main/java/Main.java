4ae37a2db31b364987a7bf95b90e1d9788a40b05
Anton <anton.tseniukh@gmail.com>
2021-10-07 23:04:08 +0300

    add save and update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,20 +7,26 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        Skill skill = new Skill();
-        skill.setId(1);
-        skill.setName("Maven");
-        Skill skill2 = new Skill();
-        skill2.setId(2);
-        skill2.setName("MongoDB");
-        String json = gson.toJson(skill);
-        String json2 = gson.toJson(skill2);
-
-        System.out.println(json);
-
-
+//        Skill skill = new Skill();
+//        skill.setId(1);
+//        skill.setName("Maven");
+//        Skill skill2 = new Skill();
+//        skill2.setId(2);
+//        skill2.setName("MongoDB");
+//        String json = gson.toJson(skill);
+//        String json2 = gson.toJson(skill2);
+//
+//        System.out.println(json);
 
         SkillRepository skillRepository = new SkillRepository();
-        skillRepository.getAll();
+//        System.out.println(skillRepository.getSkillById(2));
+//        System.out.println(skillRepository.getAll());
+//        Skill skill3 = new Skill(3,"Python");
+        Skill skill1 = new Skill(1,"C++");
+//        System.out.println(skillRepository.save(skill3));
+        skillRepository.deleteSkillById(1);
+        skillRepository.update(skill1);
+        System.out.println(skillRepository.getAll());
+
     }
 }
--- src/main/java/SkillRepository.java
+++ src/main/java/SkillRepository.java
@@ -2,11 +2,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.File;
+import java.io.*;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -16,6 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SkillRepository {
+
+
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     private static final String LOCATION = "skills.json";
@@ -23,16 +22,71 @@ public class SkillRepository {
     private URL resourseUrl = getClass().getClassLoader().getResource(LOCATION);
 
     public Skill getSkillById(Integer id) {
-        try{
+        try {
             String skillJson = Files.readString(Path.of(resourseUrl.toURI()));
-            Type type = new TypeToken<ArrayList<Skill>>() {}.getType();
-            List<Skill> skills = gson.fromJson(skillJson,type);
-            for (Skill skill : skills){
-                if (skill.getId().equals(id)){
+            Type type = new TypeToken<ArrayList<Skill>>() {
+            }.getType();
+            List<Skill> skills = gson.fromJson(skillJson, type);
+            for (Skill skill : skills) {
+                if (skill.getId().equals(id)) {
                     return skill;
                 }
             }
-        }catch (IOException | URISyntaxException e){
+        } catch (IOException | URISyntaxException e) {
+            System.out.println("Exception " + e.toString());
+        }
+        return null;
+    }
+
+    public void deleteSkillById(Integer id) {
+        try {
+            String skillJson = Files.readString(Path.of(resourseUrl.toURI()));
+            Type type = new TypeToken<ArrayList<Skill>>() {
+            }.getType();
+            List<Skill> skills = gson.fromJson(skillJson, type);
+            skills.removeIf(skill -> skill.getId().equals(id));
+            for (Skill skill : skills) {
+                System.out.println(skill);
+            }
+        } catch (IOException | URISyntaxException e) {
+            System.out.println("Exception " + e.toString());
+        }
+    }
+
+    public Skill update(Skill skill) {
+        try {
+            String skillJson = Files.readString(Path.of(resourseUrl.toURI()));
+            Type type = new TypeToken<ArrayList<Skill>>() {
+            }.getType();
+            List<Skill> skills = gson.fromJson(skillJson, type);
+            for (Skill skillArr : skills) {
+                if (skillArr.getId().equals(skill.getId())) {
+                    skillArr.setName("NewSkill");
+                }
+            }
+            String newRecord = gson.toJson(skills);
+            ObjectOutputStream obj = new ObjectOutputStream(new FileOutputStream(LOCATION));
+            obj.writeObject(newRecord);
+            System.out.println(skills);
+        } catch (IOException | URISyntaxException e) {
+            System.out.println("Exception " + e.toString());
+        }
+        return null;
+    }
+
+    public Skill save(Skill skill) {
+        try {
+            String skillJson = Files.readString(Path.of(resourseUrl.toURI()));
+            Type type = new TypeToken<ArrayList<Skill>>() {
+            }.getType();
+            List<Skill> skills = gson.fromJson(skillJson, type);
+            skills.add(skill);
+            String newRecord = gson.toJson(skills);
+            ObjectOutputStream obj = new ObjectOutputStream(new FileOutputStream(LOCATION));
+            obj.writeObject(newRecord);
+            System.out.println(skills);
+            return skill;
+        } catch (Exception e) {
             System.out.println("Exception " + e.toString());
         }
         return null;
@@ -44,25 +98,12 @@ public class SkillRepository {
             Type type = new TypeToken<ArrayList<Skill>>() {
             }.getType();
             List<Skill> skills = gson.fromJson(skillsJson, type);
-            for (Skill skill : skills) {
-                return skills;
-            }
+            return skills;
         } catch (Exception e) {
             System.out.println("Exception " + e.toString());
         }
         return null;
     }
 
-    public Skill save(Skill skill) {
-
-        try {
-            String record = gson.toJson(skill, new TypeToken<Skill>() {
-            }.getType());
-            return skill;
-        } catch (Exception e) {
-            System.out.println("Exception " + e.toString());
-        }
-        return null;
-    }
 
 }
